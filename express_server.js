f006a6b47b8dccea907454addfa2cc9d22f16ec7
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-03-20 20:05:55 +0000

    Add start of the tiny app (server and ejs file drafts

---
+++ express_server.js
@@ -0,0 +1,43 @@
+var express = require("express");
+var app = express();
+
+// Port 80 or the one my environment is using?
+var PORT = process.env.PORT || 8080;
+
+app.set("view engine", "ejs");
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+/* Pass the URL data into the TinyApp URL directory
+(i.e., urls_index.ejs template).
+*/
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+// Display a single URL and its shortened form.
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id };
+  res.render("urls_show", templateVars);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
